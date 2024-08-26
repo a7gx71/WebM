8fc8251de5443bb3641f13a681b6dd0cafcd92fb
Adolf Platto <Platto.adolf7@gmail.com>
2024-08-26 22:13:03 +0300

    first commit

---
+++ AppWebM/src/main/java/com/example/AppWebM/AppWebMApplication.java
@@ -0,0 +1,13 @@
+package com.example.AppWebM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebMApplication.class, args);
+	}
+
+}
